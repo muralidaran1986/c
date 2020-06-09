601df930f206ec3c7efe20f173901c81eefeef0a
Muralidaran Kunasundram <muralidaran@Muralidarans-MacBook-Pro.local>
2020-06-09 12:00:48 +0800

    Added a simple java program

--- src/main/java/life/circles/sampleJava/MyFirstJavaProgram.java
+++ src/main/java/life/circles/sampleJava/MyFirstJavaProgram.java
@@ -1,9 +1,7 @@
 package life.circles.sampleJava;
 
 public class MyFirstJavaProgram {
-}
-
-public static void main(String [] a){
-    System.out.println("This is first java program");   }
-
+    public static void main(String[] a) {
+        System.out.println("This is first java program");
+    }
 }
